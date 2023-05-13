4db5e5cfa876670bffb7e6d88f6b878a5ed43d29
lokeshch88 <130400983+lokeshch88@users.noreply.github.com>
2023-05-13 23:29:35 +0530

    Add files via upload

---
+++ EmpCustomException.java
@@ -0,0 +1,11 @@
+package Custom_Exc;
+
+@SuppressWarnings("serial")
+public class EmpCustomException extends Exception {
+	
+	public EmpCustomException(String errMsg) {
+		super(errMsg);
+	}
+	
+
+}
